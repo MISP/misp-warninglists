338241e4999fa618909241e7e413c9fab5debca5
Alexandre Dulaunoy <a@foo.be>
2016-12-06 12:39:45 +0100

    Add version and name to the office365 warning list

--- tools/generate-office365.py
+++ tools/generate-office365.py
@@ -4,6 +4,7 @@
 import requests
 import xml.etree.ElementTree as ET
 import json
+import datetime
 
 url = 'https://support.content.office.net/en-us/static/O365IPAddresses.xml'
 r = requests.get(url)
@@ -13,7 +14,11 @@ for address in office365.iter('address'):
     l.append(address.text.replace('*', ''))
 
 warninglist = {}
+warninglist['name'] = 'List of known Office 365 URLs and IP address ranges'
+d = datetime.datetime.now()
+warninglist['version'] = "{0}{1:02d}{2:02d}".format(d.year, d.month, d.day)
 warninglist['description'] = 'Office 365 URLs and IP address ranges'
 warninglist['list'] = l
+
 
 print (json.dumps(warninglist))
