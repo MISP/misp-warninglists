d32eb23a58d120a2ec9a7c1582b129fc39c9a649
Kevin Holvoet <kevin@wiscy.eu>
2020-07-20 17:40:00 +0200

    Chg generator-publicdns: work with new CSV format 1. The CSV format has changed with the update on 2020-07-14. 2. The script also generates IPv4, IPv6, and the hostname lists at once. 3. Downloaded file added to .gitignore

--- tools/generate-publicdns.py
+++ tools/generate-publicdns.py
@@ -1,69 +1,77 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
 import csv
-import datetime
-import logging
 import ipaddress
 import json
-import os
-import requests
+import logging
 
-servers_url = 'http://public-dns.info/nameservers.csv'
-csv_path = 'nameservers.csv'
-dns4_path = 'list4.json'
-dns6_path = 'list6.json'
-
-if os.path.isfile(csv_path):
-    logging.warning('Not erasing local csv file')
-else:
-    req = requests.get(servers_url)
-    with open(csv_path, 'wb') as fd:
-        for chunk in req.iter_content(4096):
-            fd.write(chunk)
-
-ip4_list = []
-ip6_list = []
-with open(csv_path) as csv_file:
-    servers_list = csv.reader(csv_file, delimiter=',', quotechar='"')
-
-    for row in servers_list:
-        if row[5] == '':
-            try:
-                ip = ipaddress.ip_address(row[0])
-
-                if ip.version == 4:
-                    list = ip4_list
-                else:
-                    list = ip6_list
-
-                list.append(ip.compressed)
-                if len(row[1]) > 0 and row[1] != '.':
-                    list.append(row[1])
-
-            except ValueError as exc:
-                logging.warning(str(exc))
-
-version = int(datetime.date.today().strftime('%Y%m%d'))
-
-out4_list = {}
-out4_list['name'] = 'List of known IPv4 public DNS resolvers'
-out4_list['version'] = version
-out4_list['description'] = 'Event contains one or more public IPv4 DNS resolvers as attribute with an IDS flag set'
-out4_list['matching_attributes'] = ['ip-src', 'ip-dst', 'domain|ip']
-out4_list['list'] = sorted(set(ip4_list))
+from generator import download_to_file, get_abspath_list_file, get_version
 
 
-out6_list = {}
-out6_list['name'] = 'List of known IPv6 public DNS resolvers'
-out6_list['version'] = version
-out6_list['description'] = 'Event contains one or more public IPv6 DNS resolvers as attribute with an IDS flag set'
-out6_list['matching_attributes'] = ['ip-src', 'ip-dst', 'domain|ip']
-out6_list['list'] = sorted(set(ip6_list))
+def process(file, warninglist, dst, type='v4'):
+
+    with open(file) as csv_file:
+        servers_list = csv.reader(csv_file, delimiter=',', quotechar='"')
+
+        data_list = []
+        for row in servers_list:
+            if row[7] in (None, ""):
+                try:
+                    ip = ipaddress.ip_address(row[0])
+
+                    if type == 'v4' and ip.version == 4:
+                        data_list.append(ip.compressed)
+                    elif type == 'v6' and ip.version == 6:
+                        data_list.append(ip.compressed)
+                    elif type == 'hostname' and row[1] not in (None, "", '.'):
+                        data_list.append(row[1])
+                except ValueError as exc:
+                    logging.warning(str(exc))
+
+    warninglist['version'] = get_version()
+    warninglist['list'] = sorted(set(data_list))
+
+    with open(get_abspath_list_file(dst), 'w') as data_file:
+        json.dump(warninglist, data_file, indent=2, sort_keys=True)
+        data_file.write("\n")
 
 
-# print(json.dumps(out4_list, indent=True))
-with open(dns4_path, 'w') as dns4_file:
-    dns4_file.write(json.dumps(out4_list, indent=4, sort_keys=True))
+if __name__ == '__main__':
+    publicdns_url = 'https://public-dns.info/nameservers.csv'
+    publicdns_file = 'public-dns-nameservers.csv'
 
-with open(dns6_path, 'w') as dns6_file:
-    dns6_file.write(json.dumps(out6_list, indent=4, sort_keys=True))
+    download_to_file(publicdns_url, publicdns_file)
+
+    # Public DNS Domains
+    publicdns_hostname_dst = 'public-dns-hostname'
+    publicdns_hostname_warninglist = {
+        'description': 'Event contains one or more public DNS resolvers (expressed as hostname) as attribute with an IDS flag set',
+        'name': 'List of known public DNS resolvers expressed as hostname',
+        'type': 'hostname',
+        'matching_attributes': ['hostname', 'domain', 'url', 'domain|ip']
+    }
+    process(publicdns_file, publicdns_hostname_warninglist,
+            publicdns_hostname_dst, type='hostname')
+
+    # Public DNS IPv4
+    publicdns_ipv4_dst = 'public-dns-v4'
+    publicdns_ipv4_warninglist = {
+        'description': 'Event contains one or more public IPv4 DNS resolvers as attribute with an IDS flag set',
+        'name': 'List of known IPv4 public DNS resolvers',
+        'type': 'string',
+        'matching_attributes': ['ip-src', 'ip-dst', 'domain|ip']
+    }
+    process(publicdns_file, publicdns_ipv4_warninglist,
+            publicdns_ipv4_dst, type='v4')
+
+    # Public DNS IPv4
+    publicdns_ipv6_dst = 'public-dns-v6'
+    publicdns_ipv6_warninglist = {
+        'description': 'Event contains one or more public IPv6 DNS resolvers as attribute with an IDS flag set',
+        'name': 'List of known IPv6 public DNS resolvers',
+        'type': 'string',
+        'matching_attributes': ['ip-src', 'ip-dst', 'domain|ip']
+    }
+    process(publicdns_file, publicdns_ipv6_warninglist,
+            publicdns_ipv6_dst, type='v6')
