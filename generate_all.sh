8978062f73fdea33cb44de4580acfb902621c1c9
Alexandre Dulaunoy <a@foo.be>
2021-06-10 19:05:31 +0200

    fix: [script] typo in one of the script name

--- generate_all.sh
+++ generate_all.sh
@@ -29,7 +29,7 @@ python3 generate_tranco.py
 python3 generate-university-domain-list.py
 python3 generate-vpn.py
 python3 generate-wikimedia.py
-python3 genetate-second-level-tlds.py
+python3 generate-second-level-tlds.py
 python3 generate-google-gcp.py
 python3 generate-google-gmail-sending-ips.py
 popd
