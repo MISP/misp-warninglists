0b1ec05132ee760f0d8f000d3b82bbe728f48ce3
Alexandre Dulaunoy <a@foo.be>
2023-01-02 11:25:37 +0100

    chg: [generate_all] add generate-chrome-crux-1m.py

--- generate_all.sh
+++ generate_all.sh
@@ -33,6 +33,7 @@ python3 generate-google-gmail-sending-ips.py
 python3 generate-smtp.py
 python3 generate-tenable.py
 python3 generate-microsoft-azure-appid.py
+python3 generate-chrome-crux-1m.py
 popd
 
 ./jq_all_the_things.sh
