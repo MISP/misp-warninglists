e9efbff3d53416cd6491f75cb9ccc6dd05c35f7c
Alexandre Dulaunoy <a@foo.be>
2024-04-02 14:32:29 +0200

    new: [warning-lists] add the check-host.net source of IP addresses

---
+++ tools/generate-check-host-net.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import json
+
+from generator import download_to_file, get_version, write_to_file, get_abspath_source_file, consolidate_networks
+
+
+def process(file, dst):
+    with open(get_abspath_source_file(file), 'r') as json_file:
+        checkhost_ip_list = json.load(json_file)
+    l = []
+
+    for prefix in checkhost_ip_list['nodes']:
+        l.append(prefix)
+
+    warninglist = {
+        'name': 'List of known check-host.net IP address ranges',
+        'version': get_version(),
+        'description': 'check-host IP addresses (https://check-host.net/nodes/ips)',
+        'type': 'cidr',
+        'list': consolidate_networks(l),
+        'matching_attributes': ["ip-src", "ip-dst", "domain|ip"]
+    }
+
+    write_to_file(warninglist, dst)
+
+
+if __name__ == '__main__':
+    checkhost_url = "https://check-host.net/nodes/ips"
+    checkhost_file = "check-host-net.json"
+    checkhost_dst = "check-host-net"
+
+    download_to_file(checkhost_url, checkhost_file)
+    process(checkhost_file, checkhost_dst)
