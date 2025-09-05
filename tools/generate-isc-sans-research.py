5662cf7415af86d417e09f783cad54c54e6266d5
HiSe <HiSe@github.com>
2025-09-05 13:03:40 +0200

    extension, so that the list is created as dict

--- tools/generate-isc-sans-research.py
+++ tools/generate-isc-sans-research.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import json
 import requests
 import ipaddress
 import xml.etree.ElementTree as ET
@@ -8,23 +9,25 @@ from generator import get_version, write_to_file
 
 
 if __name__ == '__main__':
-    # need explicit header with 
     headers = {
-    "User-Agent": "MISP-WarninglistBot/1.0 (contact: misp-warninglist@github.com)"
+    "User-Agent": "MyMISPBot/1.0 (contact: misp.warninglist@github.com)"
     }  
     r = requests.get("https://isc.sans.edu/api/threatcategory/research/", headers=headers)
     r.raise_for_status()
     xml_data = r.text
     
+    
     root = ET.fromstring(xml_data)
-    values = []
+    values = {}
     for item in root.findall("research"):
         ip = item.findtext("ipv4")
+        t = item.findtext("type")
+
         if not ip:
             continue
         try:
             ipaddress.ip_address(ip)
-            values.append(f"{ip}")
+            values[ip] = t if t else ""
         except ValueError:
             continue
 
@@ -33,8 +36,8 @@ if __name__ == '__main__':
         "version": get_version(),
         "description": "List of IPs from SANS ISC Research feed (https://isc.sans.edu/api/threatcategory/research/)",
         "matching_attributes": ["ip-src", "ip-dst", "ip-dst|port", "ip-src|port"],
-        "type": "cidr",
+        "type": "string",
         "list": values,
     }
-
+    
     write_to_file(warninglist, "isc-sans-research")
