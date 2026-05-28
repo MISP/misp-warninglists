8cb1bed9ae60a0a954764d1c1b78748889d98798
Alexandre Dulaunoy <a@foo.be>
2026-05-28 12:12:12 +0200

    Add Internet Cleanup Foundation scanner warninglist generator

--- generate_all.sh
+++ generate_all.sh
@@ -44,6 +44,7 @@ python3 generate-cisco-umbrella-blockpage.py
 python3 generate-zscaler.py
 python3 generate-onyphe-scanner.py
 python3 generate-modat-scanner.py
+python3 generate-internetcleanup-scanner.py
 popd
 
 ./jq_all_the_things.sh
