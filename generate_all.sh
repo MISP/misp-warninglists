52c3413946caddc2424b5f5f748f82ee08abb669
Rifa Achrinza <25147899+achrinza@users.noreply.github.com>
2026-08-23 13:36:21 +0800

    fix: disable cisco umbrella blockpage generator
    Signed-off-by: Rifa Achrinza <25147899+achrinza@users.noreply.github.com>

--- generate_all.sh
+++ generate_all.sh
@@ -39,7 +39,7 @@ python3 generate-microsoft-azure-appid.py
 python3 generate-chrome-crux-1m.py
 python3 generate-digitalside.py
 #python3 generate-gptbot.py
-python3 generate-cisco-umbrella-blockpage.py
+#python3 generate-cisco-umbrella-blockpage.py # Disabled due to failure when running in GitHub Actions
 python3 generate-zscaler.py
 python3 generate-onyphe-scanner.py
 python3 generate-modat-scanner.py
